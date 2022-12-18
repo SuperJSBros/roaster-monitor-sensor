0d895e8842dc94b024ca5c096d67c1ff5628e098
Bing <32265253+exomachine@users.noreply.github.com>
2022-12-17 19:48:51 -0500

    changed backend from ts to simple JS

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var dotenv = require("dotenv");
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-var app = (0, express_1["default"])();
-var port = process.env.PORT;
-app.get("/", function (req, res) {
-    res.send("My Coffee Roaster Monitor");
-});
-app.get("/ambient/:ambientTemp/probe/:probeTemp", function (req, res) {
-    var ambient = req.params.ambientTemp;
-    var probe = req.params.probeTemp;
-    res.json(ambient + probe);
-});
-app.listen(port, function () {
-    console.log("[SERVER]: Listening on port ".concat(port));
-});
+
+
+const app = express()
+const port = process.env.PORT
+
+app.get("/", (req, res)=>{
+    res.send("My Coffee Roaster Monitor")
+})
+app.get("/ambient/:ambientTemp/probe/:probeTemp", (req, res)=>{
+    let ambient = req.params.ambientTemp
+    let probe = req.params.probeTemp
+    res.json({"ambient temperaature":ambient, "probe temperature":probe})
+})
+
+app.listen(port, ()=>{
+    console.log(`[SERVER]: Listening on port ${port}`)
+})
