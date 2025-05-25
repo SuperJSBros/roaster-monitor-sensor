dfa0c76eed3b2c58db1f5c9d8e1b08d64ebacdb4
Bing <bx.liu@outlook.com>
2025-05-25 14:53:35 -0400

    removed code for lcd display

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # 2022-12-07 @HTSpecOps
 # Coffee Roaster Temperature Monitor Client
-from utime import sleep
-import urequests as requests
+from time import sleep
+import requests
 from machine import Pin, I2C
 import adafruit_mcp9600
-#from sh1107 import SH1107_I2C
 import network
 #import env var.
 import config
@@ -13,10 +12,9 @@ import config
 # If you experience I/O errors, try changing the frequency.
 i2c = I2C(id=1, scl=Pin(27), sda=Pin(26), freq=100000)  # type: ignore
 mcp = adafruit_mcp9600.MCP9600(i2c)
-#display = SH1107_I2C(width=128, height=64, i2c=i2c)
 led = Pin("LED", machine.Pin.OUT) # onbord LED
 
-#temperature data store
+#temperature data store - for use as a logger
 # file = open("data.csv", "w")
 
 # URL for backend route
@@ -32,11 +30,6 @@ while not wlan.isconnected():
     sleep(3)
 
 print(wlan.ifconfig())
-
-# Init Display Label
-#display.text("  Local | Probe", 0, 0, 1)
-#display.show()
-
 # HTTP REQUEST
 def sendData(probeData):
     payload = {'probe': probeData }
@@ -51,16 +44,12 @@ while True:
         temp_probe = mcp.temperature * 1.8 +32
         print('ambiant : %d    probe : %d' % (round(temp_probe), round(temp_ambiant)))
 
-        #display.fill_rect(20, 12, 120, 10, 0)
-        #display.text(str(round(temp_ambiant)), 20, 14, 2)
-        #display.text(str(round(temp_probe)), 82, 14, 2)
-        #display.show()
-
-        sendData(round(temp_probe))
+        #sendData(round(temp_probe))
         led.on()
+        print(config.ROASTER_ID)
     
         # DATA LOGGER
         #file.write(temp_ambiant + "," + temp_probe + "\r\n")
         #file.flush()
-        sleep(1)
+        sleep(5)
         led.off()
